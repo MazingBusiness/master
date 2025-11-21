ac18467fd31b3603f0962d0fec5b4ae895d264ef
MazingBusiness <mazing.store21@gmail.com>
2025-11-21 14:38:28 +0530

    Update vite.config.js final change

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// For local server
-//https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-// For git upload
 export default defineConfig({
   plugins: [react()],
-  base: "/demo_react_mazing_business_second",
-})
+  base: "/master",
+});
