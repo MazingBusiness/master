4fd3e8ed4cd3f02daec669eba77000bdcba3a9c4
MazingBusiness <mazing.store21@gmail.com>
2025-12-03 02:48:14 +0530

    Update the style Sheet and add latest HTML with Warranty pages - revice vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
 
 // --------------- WHEN BUILD OPEN THE BELOW CODE. AND CLOSE THE ABOVE CODE ----------------
 // import { defineConfig } from 'vite'
@@ -14,9 +14,9 @@ export default defineConfig({
 // });
 
 // --------------- WHEN UPLOAD THE BELOW CODE INTO GITHUB ----------------
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/demo_react_mazing_business",
-// });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+export default defineConfig({
+  plugins: [react()],
+  base: "/demo_react_mazing_business",
+});
